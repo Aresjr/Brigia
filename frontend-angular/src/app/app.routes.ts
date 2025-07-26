2cbe9406811cfc723c475f4171ab8fb14d3c7981
Ares <ares@ares.dev.br>
2025-07-26 11:21:07 -0300

    Adicionados módulos para o Login, layout principal e Pacientes e alteradas as rotas para carregá-las de acordo

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { IndexComponent } from './features/index/index.component';
+import { PacientesComponent } from './features/pacientes/pacientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: IndexComponent },
+      { path: 'pacientes', component: PacientesComponent }
+    ]
+  }
+];
