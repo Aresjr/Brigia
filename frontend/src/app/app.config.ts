c20ea6d6530093850af7efce5f5290f0c1f17399
Ares <aresnemeia@gmail.com>
2025-08-26 18:10:05 -0300

    Traduzidos os elementos do calendário

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,11 +8,35 @@ import { LucideAngularModule } from 'lucide-angular';
 import { Home, User, Calendar, LogIn, UserPlus, Stethoscope, ChevronRight, MessageSquare,
   MessageCircle, CircleUser, ChevronFirst, Users, Search, ClipboardList, Building, ChevronDown,
   ArrowDownIcon, ArrowUpIcon, ArrowUpDownIcon, Plus, ChevronLeft, HeartHandshake, Receipt, Mail, Lock, Building2,
-  Microscope, Menu, EllipsisVertical, X
+  Microscope, Menu, EllipsisVertical, X, Pencil, Trash
  } from 'lucide-angular/src/icons';
 import { provideNgxMask } from 'ngx-mask';
-import { CalendarUtils, DateAdapter } from 'angular-calendar';
+import {
+  CalendarModule,
+  CalendarUtils,
+  DateAdapter
+} from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/moment';
+import moment from 'moment';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
+moment.locale('pt-br');
+moment.updateLocale('pt-br', {
+  longDateFormat: {
+    LT: 'HH:mm', // usado para horários curtos
+    LTS: 'HH:mm:ss',
+    L: '',
+    LL: '',
+    LLL: '',
+    LLLL: ''
+  }
+});
+
+export function momentAdapterFactory() {
+  return adapterFactory(moment);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -32,11 +56,15 @@ export const appConfig: ApplicationConfig = {
       LucideAngularModule.pick({ Home, User, Calendar, LogIn, UserPlus, Stethoscope, ChevronRight,
         MessageSquare, MessageCircle, CircleUser, ChevronFirst, Users, Search, ClipboardList,
         Building, ChevronDown, ArrowDownIcon, ArrowUpIcon, ArrowUpDownIcon, Plus, ChevronLeft,
-        HeartHandshake, Receipt, Mail, Lock, Building2, Microscope, Menu, EllipsisVertical, X
-       })
+        HeartHandshake, Receipt, Mail, Lock, Building2, Microscope, Menu, EllipsisVertical, X, Pencil, Trash
+       }),
+      CalendarModule.forRoot({
+          provide: DateAdapter,
+          useFactory: momentAdapterFactory
+        }
+      )
     ),
     provideNgxMask(),
-    CalendarUtils,
-    { provide: DateAdapter, useFactory: adapterFactory }
+    CalendarUtils
   ]
 };
--- frontend/src/app/features/shared/calendario/calendario.component.ts
+++ frontend/src/app/features/shared/calendario/calendario.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalendarModule, CalendarEvent, CalendarView } from 'angular-calendar';
-import { MonthViewDay } from 'calendar-utils';
-import { startOfDay } from 'date-fns';
+import {
+  CalendarModule,
+  CalendarEvent,
+  CalendarView,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent
+} from 'angular-calendar';
+import { Subject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-calendario',
@@ -10,29 +17,151 @@ import { startOfDay } from 'date-fns';
   imports: [
     CommonModule,
     CalendarModule,
+    FormsModule,
+    LucideAngularModule
   ],
-  templateUrl: './calendario.component.html'
+  templateUrl: './calendario.component.html',
+  styleUrl: 'calendario.component.css'
 })
-export class CalendarioComponent {
-  view: CalendarView = CalendarView.Month; // inicia no mês
+export class CalendarioComponent implements OnInit {
+
+  view: CalendarView = CalendarView.Week;
+
   CalendarView = CalendarView;
 
-  viewDate: Date = new Date(); // dia atual
+  currentDate: Date = new Date();
+  hourSegments: number = 2;
 
-  events: CalendarEvent[] = [
+  actions: CalendarEventAction[] = [
     {
-      start: startOfDay(new Date()),
-      title: 'Evento de hoje',
-      allDay: true,
+      label: '<i class="fas fa-fw fa-trash-alt w-4 h-4 block"><lucide-icon name="pencil" class="block w-4 h-4"></lucide-icon></i>',
+      a11yLabel: 'Editar',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      },
+    },
+    {
+      label: '<i class="fas fa-fw fa-trash-alt"></i>',
+      a11yLabel: 'Delete',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter((iEvent) => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      },
     },
   ];
+
+  refresh = new Subject<void>();
+
+  events: CalendarEvent[] = [];
+
+  activeDayIsOpen: boolean = true;
+
+  ngOnInit(): void {
+    let startDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 11);
+    let endDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 13);
+    this.events.push(
+      {
+        start: startDate,
+        end: endDate,
+        title: 'João da Silva - Ortopedista | Dr César',
+        color: { primary: '#1e90ff', secondary: '#D1E8FF'},
+        actions: this.actions,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true,
+        },
+        draggable: true,
+      }
+    );
+
+    startDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 12);
+    endDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 12, 30);
+    this.events.push(
+      {
+        start: startDate,
+        end: endDate,
+        title: 'João da Silva - Ortopedista | Dr César',
+        color: { primary: '#1e90ff', secondary: '#D1E8FF'},
+        actions: this.actions,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true,
+        },
+        draggable: true,
+      }
+    );
+
+    startDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 12);
+    endDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 12, 45);
+    this.events.push(
+      {
+        start: startDate,
+        end: endDate,
+        title: 'João da Silva - Ortopedista | Dr César',
+        color: { primary: '#1e90ff', secondary: '#D1E8FF'},
+        actions: this.actions,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true,
+        },
+        draggable: true,
+      }
+    );
+  }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    console.log('dayClicked');
+    console.log(date);
+    console.log(events);
+    if (this.isSameMonth(date, this.currentDate)) {
+      if ((this.isSameDay(this.currentDate, date) && this.activeDayIsOpen) || events.length === 0) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.currentDate = date;
+    }
+  }
+
+  eventTimesChanged({event, newStart, newEnd}: CalendarEventTimesChangedEvent): void {
+    this.events = this.events.map((iEvent) => {
+      if (iEvent === event) {
+        return {
+          ...event,
+          start: newStart,
+          end: newEnd,
+        };
+      }
+      return iEvent;
+    });
+    this.handleEvent('Dropped or resized', event);
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+
+    console.log(event);
+    console.log(action);
+
+  }
 
   setView(view: CalendarView) {
     this.view = view;
   }
 
-  dayClicked({ day }: { day: MonthViewDay<any> }): void {
-    this.viewDate = day.date;
-    this.view = CalendarView.Day;
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+
+  handleHour($event: { date: Date; sourceEvent: MouseEvent }) {
+    console.log('Clicou em um horário');
+    console.log($event);
+  }
+
+  isSameMonth(date1: Date, date2: Date): boolean {
+    return date1.getMonth() == date2.getMonth();
+  }
+
+  private isSameDay(date1: Date, date2: Date) {
+    return date1.getDate() == date2.getDate();
   }
 }
