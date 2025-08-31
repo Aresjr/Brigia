d5aba4ed43762be8fe2df460072f150800f70cc7
Ares <aresnemeia@gmail.com>
2025-08-31 16:40:24 -0300

    Ajuste de script

--- backend/src/main/resources/db/migration/V33__agendamento_convenio_id.sql
+++ backend/src/main/resources/db/migration/V33__agendamento_convenio_id.sql
@@ -2,6 +2,7 @@ ALTER TABLE agendamento ALTER COLUMN paciente_id SET NOT NULL;
 ALTER TABLE agendamento ALTER COLUMN profissional_id SET NOT NULL;
 
 ALTER TABLE agendamento ADD COLUMN IF NOT EXISTS unidade_id bigint;
-
 ALTER TABLE agendamento ALTER COLUMN unidade_id SET NOT NULL;
+
+ALTER TABLE agendamento ADD COLUMN IF NOT EXISTS duracao INTEGER;
 ALTER TABLE agendamento ALTER COLUMN duracao SET NOT NULL;
