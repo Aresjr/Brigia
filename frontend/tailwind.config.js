92a056222c4f81e0b7dfb4c3316e7c50f05a7b2a
Ares <aresnemeia@gmail.com>
2025-09-08 08:47:52 -0300

    Ajustados uso de classes do DaisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -31,18 +31,18 @@ module.exports = {
   ],
   daisyui: {
     themes: [
-      "dark",
+      "dark", "light", "emerald", "fantasy",
       {
         brigida: {
           "primary": "#2563eb",     // Azul principal (confiança)
           "secondary": "#10b981",   // Verde suave (saúde)
           "accent": "#60a5fa",      // Azul claro para destaques
-          "neutral": "#f3f4f6",     // Cinza escuro para textos fortes
-          "neutral-content": "#1f2937", // cinza escuro para contraste
+          "neutral": "#9ca3af",          // cinza médio
+          "neutral-content": "#ffffff",  // texto branco em cima do cinza médio
           "base-content": "#1f2937", // texto principal (preto/cinza escuro)
           "base-100": "#ffffff",    // Fundo claro
           "base-200": "#f3f4f6",    // Fundo alternativo
-          "base-300": "#e5e7eb",    // Bordas e divisores
+          "base-300": "#d1d5db",    // Bordas e divisores
           "info": "#38bdf8",        // Azul claro para info
           "success": "#22c55e",     // Verde sucesso
           "success-content": "#ffffff",
