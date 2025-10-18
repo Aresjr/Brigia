3311ab90299bd03cb9a9bbe834579815da3ddb15
Ares <aresnemeia@gmail.com>
2025-10-17 23:16:20 -0300

    Correção para o teste de componente conseguir rodar no Github Actions

--- backend/src/main/java/br/com/nemeia/brigia/service/EmailService.java
+++ backend/src/main/java/br/com/nemeia/brigia/service/EmailService.java
@@ -1,7 +1,6 @@
 package br.com.nemeia.brigia.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@Profile("!test")
 public class EmailService {
 
     private final MailgunClient mailgunClient;
